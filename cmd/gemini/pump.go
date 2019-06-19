468191933d32c1591ca1ae8b6f12711e2dc5e41b
Henrik Johansson <henrik@scylladb.com>
2019-06-19 14:51:39 +0200

    gemini: allows inflight ops to finish
    A "pump" concept is introduced with the purpose of being the force
    driving the operations against the stores. This pump exposes a
    channel that will continously emit heartbeats that has an optional
    sleep parameter telling the currently operating goroutine to sleep
    for the provided duration. When the pumps channel is closed all
    listening jobs will finish the currently available heartbeats in a
    race free manner. To speed up the process, the pump also tries to
    drain any remaining heartbeats.

---
+++ cmd/gemini/pump.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/briandowns/spinner"
+
+	"golang.org/x/net/context"
+)
+
+type Pump struct {
+	ch     chan heartBeat
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func (p *Pump) Start(postFunc func()) {
+	go func() {
+		for {
+			select {
+			case <-p.ctx.Done():
+				close(p.ch)
+				for range p.ch {
+				}
+				postFunc()
+				return
+			default:
+				p.ch <- newHeartBeat()
+			}
+		}
+	}()
+}
+
+func (p *Pump) Stop() {
+	p.cancel()
+}
+
+func createPump(sz int, d time.Duration) *Pump {
+	// Gracefully terminate
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM, syscall.SIGINT)
+
+	// Create the actual pump
+	pumpCh := make(chan heartBeat, sz)
+	pumpCtx, pumpCancel := context.WithCancel(context.Background())
+	pump := &Pump{
+		ch:     pumpCh,
+		ctx:    pumpCtx,
+		cancel: pumpCancel,
+	}
+	go func(d time.Duration) {
+		timer := time.NewTimer(d)
+		for {
+			select {
+			case <-gracefulStop:
+				pump.Stop()
+				fmt.Println("Test run aborted. Exiting.")
+				return
+			case <-timer.C:
+				pump.Stop()
+				fmt.Println("Test run completed. Exiting.")
+				return
+			}
+		}
+	}(duration + warmup)
+	return pump
+}
+
+func createPumpCallback(c chan Status, wg *sync.WaitGroup, sp *spinner.Spinner) func() {
+	return func() {
+		if sp != nil {
+			sp.Stop()
+		}
+		wg.Wait()
+		close(c)
+	}
+}
