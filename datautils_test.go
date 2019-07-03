61315d8552675a0861a17ae4dab51bb6cd6e58e3
Henrik Johansson <henrik@scylladb.com>
2019-07-03 11:34:09 +0200

    schema: removed unused data gen functions

--- datautils_test.go
+++ datautils_test.go
@@ -1,7 +1,6 @@
 package gemini
 
 import (
-	"testing"
 	"time"
 
 	"golang.org/x/exp/rand"
@@ -10,19 +9,3 @@ import (
 var rnd = rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
 
 var bench_r string
-
-func BenchmarkNonEmptyRandStringWithTime(b *testing.B) {
-	tt := time.Now()
-	for i := 0; i < b.N; i++ {
-		bench_r = nonEmptyRandStringWithTime(rnd, 30, tt)
-	}
-}
-
-func BenchmarkNonEmptyRandStringWithTimeParallel(b *testing.B) {
-	tt := time.Now()
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			bench_r = nonEmptyRandStringWithTime(rnd, 30, tt)
-		}
-	})
-}
