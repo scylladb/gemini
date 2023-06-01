51b92e87b238a3b6d57f33a2b6572f431f83d407
Dmitry Kropachev <dmitry.kropachev@scylladb.com>
2023-06-01 16:16:24 -0400

    fix(columns): add remove method

--- pkg/testschema/columns_test.go
+++ pkg/testschema/columns_test.go
@@ -16,6 +16,7 @@ package testschema_test
 
 import (
 	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -137,5 +138,74 @@ func TestMarshalUnmarshal(t *testing.T) {
 
 	if diff := cmp.Diff(s1, s2, opts); diff != "" {
 		t.Errorf("schema not the same after marshal/unmarshal, diff=%s", diff)
+	}
+}
+
+func TestPrimitives(t *testing.T) {
+	sc := &typedef.SchemaConfig{
+		MaxPartitionKeys:  3,
+		MinPartitionKeys:  2,
+		MaxClusteringKeys: 3,
+		MinClusteringKeys: 2,
+		MaxColumns:        3,
+		MinColumns:        2,
+		MaxTupleParts:     2,
+		MaxUDTParts:       2,
+	}
+
+	cols := testschema.Columns{
+		&testschema.ColumnDef{
+			Name: "pk_mv_0",
+			Type: generators.GenListType(sc),
+		},
+		&testschema.ColumnDef{
+			Name: "pk_mv_1",
+			Type: generators.GenTupleType(sc),
+		},
+		&testschema.ColumnDef{
+			Name: "ct_1",
+			Type: &coltypes.CounterType{},
+		},
+	}
+	if cols.Len() != 3 {
+		t.Errorf("%d != %d", cols.Len(), 3)
+	}
+	colNames := strings.Join(cols.Names(), ",")
+	if colNames != "pk_mv_0,pk_mv_1,ct_1" {
+		t.Errorf("%s != %s", colNames, "pk_mv_0,pk_mv_1,ct_1")
+	}
+	if cols.NonCounters().Len() != 2 {
+		t.Errorf("%d != %d", cols.NonCounters().Len(), 2)
+	}
+	colNames = strings.Join(cols.NonCounters().Names(), ",")
+	if colNames != "pk_mv_0,pk_mv_1" {
+		t.Errorf("%s != %s", colNames, "pk_mv_0,pk_mv_1")
+	}
+
+	cols = cols.Remove(2)
+	if cols.Len() != 2 {
+		t.Errorf("%d != %d", cols.Len(), 2)
+	}
+	colNames = strings.Join(cols.Names(), ",")
+	if colNames != "pk_mv_0,pk_mv_1" {
+		t.Errorf("%s != %s", colNames, "pk_mv_0,pk_mv_1")
+	}
+
+	cols = cols.Remove(0)
+	if cols.Len() != 1 {
+		t.Errorf("%d != %d", cols.Len(), 1)
+	}
+	colNames = strings.Join(cols.Names(), ",")
+	if colNames != "pk_mv_1" {
+		t.Errorf("%s != %s", colNames, "pk_mv_1")
+	}
+
+	cols = cols.Remove(0)
+	if cols.Len() != 0 {
+		t.Errorf("%d != %d", cols.Len(), 0)
+	}
+	colNames = strings.Join(cols.Names(), ",")
+	if colNames != "" {
+		t.Errorf("%s != %s", colNames, "")
 	}
 }
